cef137322a81151a623674cb710dde81d2ed52a7
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-04 23:28:36 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_141016_ArrizalFathinAthallah/week01/GameStore.kt
+++ src/oop_141016_ArrizalFathinAthallah/week01/GameStore.kt
@@ -3,6 +3,9 @@ package oop_141016_ArrizalFathinAthallah.week01
 
 fun main() {
     val gameTitle : String = "Apex Legends"
-    val price: Int = 200000
+    val price: Int = 620000
 
+    println(calculateDiscount(price))
 }
+
+fun calculateDiscount(price: Int) : Int = if (price > 500000) price-price*20/100 else price-price*10/100
