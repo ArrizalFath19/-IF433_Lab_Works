42a4a373dd7af188936a09ad53e7d8ab280c544b
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-04 23:24:48 +0700

    week01: (tugas 2) setup GameStore file

--- src/oop_141016_ArrizalFathinAthallah/week01/CircleCalculate.kt
+++ src/oop_141016_ArrizalFathinAthallah/week01/CircleCalculate.kt
@@ -1,2 +1,21 @@
 package oop_141016_ArrizalFathinAthallah.week01
 
+fun main(args: Array<String>) {
+
+    val radius = 7.0
+    val pi = 3.14
+
+    val area: Double = pi * radius * radius
+
+    println("Radius: $radius, Area:  $area")
+
+    checkSize(area)
+}
+
+fun checkSize(area: Double) {
+    if (area > 100) {
+        println("This is a Big Circle")
+    } else {
+        println("This is a Small Circle")
+    }
+}
---
+++ src/oop_141016_ArrizalFathinAthallah/week01/GameStore.kt
@@ -0,0 +1,8 @@
+package oop_141016_ArrizalFathinAthallah.week01
+
+
+fun main() {
+    val gameTitle : String = "Apex Legends"
+    val price: Int = 200000
+
+}
