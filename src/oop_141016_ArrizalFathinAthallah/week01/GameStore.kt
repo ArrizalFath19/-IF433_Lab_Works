03584f1febc54ed5825f5e97d5981bf4e4ee64d7
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-04 23:34:08 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_141016_ArrizalFathinAthallah/week01/GameStore.kt
+++ src/oop_141016_ArrizalFathinAthallah/week01/GameStore.kt
@@ -5,7 +5,11 @@ fun main() {
     val gameTitle : String = "Apex Legends"
     val price: Int = 620000
 
-    println(calculateDiscount(price))
+    printReceipt(title = gameTitle, priceAwa = price, priceakhir = calculateDiscount(price))
 }
 
 fun calculateDiscount(price: Int) : Int = if (price > 500000) price-price*20/100 else price-price*10/100
+
+fun printReceipt(title: String,priceAwal: Int, priceakhir: Int) {
+    println("Judul: $title, Harga Sebelum Diskon: $priceAwal, Harga Sesudah Diskon: $priceakhir ")
+}
