aab1384ba5955d3476b73f3ba5d3376a45602502
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-11 21:36:37 +0700

    week02: implement selection logic for constructors

--- src/oop_141016_ArrizalFathinAthallah/week02/Main.kt
+++ src/oop_141016_ArrizalFathinAthallah/week02/Main.kt
@@ -1,14 +1,14 @@
 package oop_141016_ArrizalFathinAthallah.week02
 
-import java.until.scanner
+import java.util.Scanner
 
 fun main() {
-    val scanner = Scanner(source = System. 'in')
+    val scanner = Scanner( System. `in`)
 
     println ("--- APLIKASI PMB UMN ---")
 
     print("Masukan Nama: ")
-    val name = scannner.nextLine()
+    val name = scanner.nextLine()
 
     print("Masukan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
@@ -19,10 +19,25 @@ fun main() {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 Karakter!")
 
     } else {
-        print("Masukan Jurusan: ")
-        val major = scanner.nextLine()
 
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+        print("Pilih Jalur(1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+            scanner.nextLine()
+
+            if (type == 1) {
+                print("Masukkan Jurusan: ")
+                val major = scanner.nextLine()
+
+                val s1 = Student(name, nim, major)
+                println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+
+            } else if (type == 2) {
+                val s2 = Student(name, nim)
+                println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+
+            } else {
+                println("Pilihan ngawur, pendaftaran batal!")
+            }
     }
 }
+
