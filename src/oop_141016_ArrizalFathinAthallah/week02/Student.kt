b9be355126954290367c035268b74fc250614855
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-11 20:37:16 +0700

    week02: add warning validation in init block

--- src/oop_141016_ArrizalFathinAthallah/week02/Student.kt
+++ src/oop_141016_ArrizalFathinAthallah/week02/Student.kt
@@ -7,3 +7,14 @@ class Student (
 ){
     // Body kosong
 }
+
+class Student (val name: String, val nim: String, val major: String) {
+    init {
+        if (nim.length !=5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid! ")
+            println("Data mahasiwa $name mungkin akan bermasalah di sistem. ")
+        }else {
+            println("LOG : Objek Student $name berhasil dialokasikan di Memory. ")
+        }
+    }
+}
