1cd6dadd388eb280ec10e26ee5538d83b1796606
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-11 21:40:18 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_141016_ArrizalFathinAthallah/week02/Student.kt
+++ src/oop_141016_ArrizalFathinAthallah/week02/Student.kt
@@ -1,24 +1,22 @@
 package oop_141016_ArrizalFathinAthallah.week02
 
-class Student (
-    var name : String,
-    var nim : String,
-    var major : String,
-    var gpa : Double, = 0.0
-){
-    // Body kosong
-}
-
-class Student (val name: String, val nim: String, val major: String) {
+class Student(
+    val name: String,
+    val nim: String,
+    val major: String,
+    var gpa: Double = 0.0
+) {
     init {
-        if (nim.length !=5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid! ")
-            println("Data mahasiwa $name mungkin akan bermasalah di sistem. ")
-        }else {
-            println("LOG : Objek Student $name berhasil dialokasikan di Memory. ")
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
-    constructor(name: String, nim: String,) : this(name, nim, major = "Non-Matriculated"){
+
+    constructor(name: String, nim: String) :
+            this(name, nim, major = "Non-Matriculated") {
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan)")
     }
 }
