7d7ed21079d01843f52d73814d41b83b64222389
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-12 10:40:58 +0700

    week03: add private property

--- src/oop_141016_ArrizalFathinAthallah/week03/Employee.kt
+++ src/oop_141016_ArrizalFathinAthallah/week03/Employee.kt
@@ -4,10 +4,20 @@ class Employee(val name : String) {
     var salary : Int = 0
         set(value) {
             if(value < 0) {
-                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0."
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
             } else {
                 field = value
             }
         }
+    private  var performanceRating: Int = 3
 
+    fun increasePerformanceRating() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating" )
+    }
+
+    fun printStatus() {
+        println("Karyawan $name, Rating: $performanceRating")
+    }
 }
+
