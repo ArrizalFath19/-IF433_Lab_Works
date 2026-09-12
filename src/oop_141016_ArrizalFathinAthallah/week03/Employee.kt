afe3a08a42e0e6d7c9732de8e68caef2f993e7b8
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-12 10:27:47 +0700

    week03: simulate stack overflow error in setter

--- src/oop_141016_ArrizalFathinAthallah/week03/Employee.kt
+++ src/oop_141016_ArrizalFathinAthallah/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_141016_ArrizalFathinAthallah.week03
 
 class Employee(val name : String) {
     var salary : Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_141016_ArrizalFathinAthallah/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_141016_ArrizalFathinAthallah.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
