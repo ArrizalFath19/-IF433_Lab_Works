9169f79028b45bbac34b830e28613efa9f09f6a7
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-18 21:23:45 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_141016_ArrizalFathinAthallah/week03/MainWeapon.kt
+++ src/oop_141016_ArrizalFathinAthallah/week03/MainWeapon.kt
@@ -4,4 +4,11 @@ fun main() {
     val weapon = Weapon("Excalibur")
 
     weapon.damage = -50
+    println("Damage: ${weapon.damage}")
+
+    weapon.damage = 9999
+    println("Damage: ${weapon.damage}")
+
+    println("Nama Weapon: ${weapon.name}")
+    println("Tier: ${weapon.tier}")
 }
