1390df0806250ceb78d1a8edea63aa9e05a38ec2
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-19 10:13:40 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_141016_ArrizalFathinAthallah/week04/Car.kt
@@ -0,0 +1,8 @@
+package oop_141016_ArrizalFathinAthallah.week04
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka")
+    }
+}
