08da402c441698a463cced54ef03a05b8fa1799a
ArrizalFath19 <arrizal.fathin@student.umn.ac.id>
2026-09-19 20:15:14 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_141016_ArrizalFathinAthallah/week04/Employee.kt
@@ -0,0 +1,10 @@
+package oop_141016_ArrizalFathinAthallah.week04
+
+open class Employee (val name: String, var baseSalary: Int) {
+    open fun work("$name sedang bekerja.") {
+        println("$name sedang bekerja")
+    }
+    open fun calculateBonus(): Int{
+        return (baseSalary * 10) / 100
+    }
+}
